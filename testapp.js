f84db791e2898aed60ee5a073c3a683e2a7e7427
James Harvey <jmharvey@videable.com>
2013-03-04 00:45:49 +0000

    broken stuff, saving here to revert

--- testapp.js
+++ testapp.js
@@ -70,19 +70,33 @@ if (Meteor.isClient) {
     };
 
 
-
     Template.races.race = function () {
 	return Races.find({}, {sort: {raceNumber: 1}});
     };
     
     Template.races.otherPlayersHeader = function () {
-//	Meteor.users.find({_id: {$not: Meteor.userId()}}).map(function (user) {return user.profile ? user.profile.name : user.emails[0].address || ''});
-	return '';
+	var string = Meteor.users.find({_id: {$not: Meteor.userId()}}, {sort: {_id: 1}}).map(function (user) {
+	    var displayName = user.profile ? user.profile.name : user.emails ? user.emails[0].address : '';
+	    return '<th>' + displayName + '</th>';
+	}).join('');
+	console.log(string);
+	return string;
     };
 
     Template.race.locked = function () {
 	var raceDate = this.date;
 	return raceDate < new Date();
+    };
+
+    Template.race.otherDriverPicks = function (raceId) {
+	var otherUserIds = Meteor.users.find({_id: {$not: Meteor.userId()}}, {sort: {_id: 1}}).map(function (user) {return user._id});
+	var tds = _.map(otherUserIds, function (userId) {
+	    var pick = Picks.find({racename: raceId, owner: userId}).count() ?
+		Picks.findOne({racename: raceId, owner: userId}).pick
+	    : 'No pick made';
+	    return '<td>' + pick + '</td>';
+	});
+	return tds.join('');
     };
 
     Template.race.placeholder = function () {
@@ -167,43 +181,9 @@ if (Meteor.isServer) {
 	  return Meteor.users.find({});
       });
 
+
       if (Races.find().count() === 0) {
-      var races = [["Sunday","24-Feb","Daytona 500","Daytona",1],
-		   ["Sunday","3-Mar","Subway 500","Phoenix",1],
-		   ["Sunday","10-Mar","Kobalt Tools 400","Las Vegas",1],
-		   ["Sunday","17-Mar","Ford City 500","Bristol",1],
-		   ["Sunday","24-Mar","Auto Club 400","Fontana",1],
-		   ["Sunday","7-Apr","Virginia 500","Martinsville",1],
-		   ["Saturday","13-Apr","Texas 500","Texas",1],
-		   ["Sunday","21-Apr","STP 400","Kansas",1],
-		   ["Saturday","27-Apr","Toyota 400","Richmond",1],
-		   ["Sunday","5-May","Aaron\'s 499","Talladega",1],
-		   ["Saturday","11-May","Southern 500","Darlington",1],
-		   ["Sunday","26-May","Coca-Cola 600","Charlotte",1],
-		   ["Sunday","2-Jun","Dover 400","Dover",2],
-		   ["Sunday","9-Jun","Pocono 400","Pocono",2],
-		   ["Sunday","16-Jun","Quicken 400","Michigan",2],
-		   ["Sunday","23-Jun","Toyota/Save Mart 350","Sonoma",2],
-		   ["Saturday","29-Jun","Quaker State 400","Kentucky",2],
-		   ["Saturday","6-Jul","Coke Zero 400","Daytona",2],
-		   ["Sunday","14-Jul","New Hampshire 300","New Hampshire",2],
-		   ["Sunday","28-Jul","Crown Royal 400","Indianapolis",2],
-		   ["Sunday","4-Aug","Pennsylvania 400","Pocono",2],
-		   ["Sunday","11-Aug","Cheez-It 355 at the Glen","Watkins Glen",2],
-		   ["Sunday","18-Aug","Pure Michigan 400","Michigan",2],
-		   ["Saturday","24-Aug","Irwin Tools Night Race","Bristol",2],
-		   ["Sunday","1-Sep","AdvoCare (1) 500","Atlanta",2],
-		   ["Saturday","7-Sep","Fed Auto Parts 400","Richmond",2],
-		   ["Sunday","15-Sep","Geico 400","Chicagoland",3],
-		   ["Sunday","22-Sep","Sylvania 300","New Hampshire",3],
-		   ["Sunday","29-Sep","AAA 400","Dover",3],
-		   ["Sunday","6-Oct","Hollywood Casino 400","Kansas",3],
-		   ["Saturday","12-Oct","B of A 500","Charlotte",3],
-		   ["Sunday","20-Oct","Camping World 500","Talladega",3],
-		   ["Sunday","27-Oct","Goody\'s 500","Martinsville",3],
-		   ["Sunday","3-Nov","AAA Texas 500","Texas",3],
-		   ["Sunday","10-Nov","AdvoCare (2) 500","Phoenix",3],
-		   ["Sunday","17-Nov","Ford 400","Homestead",3]];
+	  var races = [["Sunday","24-Feb","Daytona 500","Daytona",1,1],["Sunday","3-Mar","Subway 500","Phoenix",1,53],["Sunday","10-Mar","Kobalt Tools 400","Las Vegas",1,28],["Sunday","17-Mar","Ford City 500","Bristol",1,31],["Sunday","24-Mar","Auto Club 400","Fontana",1,26],["Sunday","7-Apr","Virginia 500","Martinsville",1,15],["Saturday","13-Apr","Texas 500","Texas",1,32],["Sunday","21-Apr","STP 400","Kansas",1,55],["Saturday","27-Apr","Toyota 400","Richmond",1,5],["Sunday","5-May","Aaron's 499","Talladega",1,22],["Saturday","11-May","Southern 500","Darlington",1,30],["Sunday","26-May","Coca-Cola 600","Charlotte",1,3],["Sunday","2-Jun","Dover 400","Dover",2,4],["Sunday","9-Jun","Pocono 400","Pocono",2,7],["Sunday","16-Jun","Quicken 400","Michigan",2,6],["Sunday","23-Jun","Toyota/Save Mart 350","Sonoma",2,8],["Saturday","29-Jun","Quaker State 400","Kentucky",2,56],["Saturday","6-Jul","Coke Zero 400","Daytona",2,9],["Sunday","14-Jul","New Hampshire 300","New Hampshire",2,10],["Sunday","28-Jul","Crown Royal 400","Indianapolis",2,12],["Sunday","4-Aug","Pennsylvania 400","Pocono",2,11],["Sunday","11-Aug","Cheez-It 355 at the Glen","Watkins Glen",2,13],["Sunday","18-Aug","Pure Michigan 400","Michigan",2,14],["Saturday","24-Aug","Irwin Tools Night Race","Bristol",2,16],["Sunday","1-Sep","AdvoCare (1) 500","Atlanta",2,25],["Saturday","7-Sep","Fed Auto Parts 400","Richmond",2,18],["Sunday","15-Sep","Geico 400","Chicagoland",3,38],["Sunday","22-Sep","Sylvania 300","New Hampshire",3,33],["Sunday","29-Sep","AAA 400","Dover",3,19],["Sunday","6-Oct","Hollywood Casino 400","Kansas",3,39],["Saturday","12-Oct","B of A 500","Charlotte",3,21],["Sunday","20-Oct","Camping World 500","Talladega",3,41],["Sunday","27-Oct","Goody's 500","Martinsville",3,20],["Sunday","3-Nov","AAA Texas 500","Texas",3,54],["Sunday","10-Nov","AdvoCare (2) 500","Phoenix",3,23],["Sunday","17-Nov","Ford 400","Homestead",3,42]];
 	  for (var i = 0; i < races.length; i++){
 	      var dateString;
 	      if (races[i][0] == "Saturday") {
@@ -217,10 +197,13 @@ if (Meteor.isServer) {
 			    date: new Date(dateString),
 			    track: races[i][3],
 			    seasonPart: races[i][4],
-			    raceNumber: i+1}); 
+			    yahooId: races[i][5],
+			    raceNumber: i+1});
 	  }
-      }
+      };
 
+      var result = Meteor.http.get("http://sports.yahoo.com/nascar/sprint/races/1/qualify");
+      Races.update({yahooId: 1}, {result: result});
   
       if (Drivers.find().count() === 0) {
 	  var drivers = ["J McMurray",
